c8fba7c3b9fe81b1a2ba3418952740d50c3c6471
Satrajit Ghosh <satra@mit.edu>
2020-05-04 10:59:37 -0400

    enh: update redirects and add some sanic logging

--- serve.py
+++ serve.py
@@ -1,34 +1,110 @@
 import asyncio
-
+import os
 from sanic import Sanic
-from sanic.log import logger, error_logger, access_logger
+from sanic.log import logger
 from sanic import response
-from sanic_cors import CORS, cross_origin
+from sanic_cors import CORS
 import requests
-from sanic.exceptions import abort
 
-app = Sanic("redirector")
+LOG_SETTINGS = dict(
+    version=1,
+    disable_existing_loggers=False,
+    loggers={
+        "sanic.root": {"level": "INFO", "handlers": ["consolefile"]},
+        "sanic.error": {
+            "level": "INFO",
+            "handlers": ["error_consolefile"],
+            "propagate": True,
+            "qualname": "sanic.error",
+        },
+        "sanic.access": {
+            "level": "INFO",
+            "handlers": ["access_consolefile"],
+            "propagate": True,
+            "qualname": "sanic.access",
+        },
+    },
+    handlers={
+        "consolefile": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "when": 'D',
+            "interval": 7,
+            "backupCount": 10,
+            'filename': os.path.join(basedir, "redirector", "console.log"),
+            "formatter": "generic",
+        },
+        "error_consolefile": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "when": 'D',
+            "interval": 7,
+            "backupCount": 10,
+            'filename': os.path.join(basedir, "redirector", "error.log"),
+            "formatter": "generic",
+        },
+        "access_consolefile": {
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "when": 'D',
+            "interval": 7,
+            "backupCount": 10,
+            'filename': os.path.join(basedir, "redirector", "access.log"),
+            "formatter": "access",
+        },
+    },
+    formatters={
+        "generic": {
+            "format": "%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+        "access": {
+            "format": "%(asctime)s - (%(name)s)[%(levelname)s][%(host)s]: "
+                      + "%(request)s %(message)s %(status)d %(byte)d",
+            "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+            "class": "logging.Formatter",
+        },
+    },
+)
+
+production = True and 'DEV628cc89a6444' not in os.environ
+sem = None
+basedir = os.environ["HOME"] if production else os.getcwd()
+
+if production:
+    app = Sanic("redirector", log_config=LOG_SETTINGS)
+else:
+    app = Sanic("redirector")
 CORS(app)
+
 
 @app.listener("before_server_start")
 async def init(app, loop):
-    app.sem = asyncio.Semaphore(100, loop=loop)
+    global sem
+    sem = asyncio.Semaphore(100)
 
 
 @app.route("/")
 async def main(request):
-    return response.redirect("https://www.dandiarchive.org/")
+    return response.redirect("https://gui.dandiarchive.org/")
+
 
 @app.route("/about")
 async def about(request):
     return response.redirect("https://www.dandiarchive.org")
 
 
+@app.route("/dandiset")
+async def get_public_dashboard(request):
+    """Redirect to gui draft collection
+    """
+    return response.redirect("https://gui.dandiarchive.org/#/dandiset")
+
+
 @app.route("/dandiset/drafts")
 async def get_draft_collection(request):
     """Redirect to gui draft collection
     """
-    return response.redirect("https://gui.dandiarchive.org/#/collection/5e59bb0af19e820ab6ea6c62")
+    return response.redirect("/dandiset", status=301)
+
 
 @app.route("/dandiset/<dataset:int>")
 async def get_dataset_info(request, dataset):
@@ -40,8 +116,9 @@ async def get_dataset_info(request, dataset):
         json_info = req.json()
         if json_info is not None:
             id = json_info['_id']
-            return response.redirect(f"https://gui.dandiarchive.org/#/dandiset-meta/{id}")
+            return response.redirect(f"https://gui.dandiarchive.org/#/dandiset/{id}")
     return response.text(f"dandi:{dataset:06d} not found.", status=404)
+
 
 @app.route("/dandiset/<dataset:int>/<version>")
 async def get_dataset_version_info(request, dataset, version):
@@ -51,7 +128,7 @@ async def get_dataset_version_info(request, dataset, version):
         json_info = req.json()
         if json_info is not None:
             id = json_info['_id']
-            return response.redirect(f"https://gui.dandiarchive.org/#/dandiset-meta/{id}")
+            return response.redirect(f"https://gui.dandiarchive.org/#/dandiset/{id}")
     return response.text(f"dandi:{dataset:06d}/{version} not found.", status=404)
 
 if __name__ == "__main__":
