01dc41247df34b5ff12df44c7454e4186cc77d6b
Satrajit Ghosh <satrajit.ghosh@gmail.com>
2020-08-07 12:47:59 -0400

    Apply suggestions from code review
    Adding versioned dandiset tests

--- test_serve.py
+++ test_serve.py
@@ -11,10 +11,10 @@ NEXIST_DANDI_ID = "999999"
         "/dandiset/000003",
         "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
-    #(
-    #    "/dandiset/000003/???",
-    #    "https://gui.dandiarchive.org/#/dandiset/???",
-    #),
+    (
+    "/dandiset/000003/0.20200703.1040",
+    "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
+    ),
 ])
 def test_redirect(req_url, resp_url):
     _, r = app.test_client.get(req_url)
@@ -26,10 +26,10 @@ def test_redirect(req_url, resp_url):
         "/dandiset/000003",
         "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
-    #(
-    #    "/dandiset/000003/???",
-    #    "https://gui.dandiarchive.org/#/dandiset/???",
-    #),
+    (
+    "/dandiset/000003/0.20200703.1040",
+    "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
+    ),
 ])
 def test_redirect_head(req_url, resp_url):
     _, r = app.test_client.head(req_url)
