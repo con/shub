8d442cb50395269cc37742e02b7e8e05a5051089
John T. Wodder II <git@varonathe.org>
2020-08-07 12:21:04 -0400

    Fix test requests

--- test_serve.py
+++ test_serve.py
@@ -1,19 +1,18 @@
 import pytest
 from   serve import app
 
-REDIRECTOR = "http://localhost:8080"
 NEXIST_DANDI_ID = "999999"
 
 @pytest.mark.parametrize('req_url,resp_url', [
-    (REDIRECTOR, "https://gui.dandiarchive.org/"),
-    (f"{REDIRECTOR}/about", "https://www.dandiarchive.org"),
-    (f"{REDIRECTOR}/dandiset", "https://gui.dandiarchive.org/#/dandiset"),
+    ("/", "https://gui.dandiarchive.org/"),
+    ("/about", "https://www.dandiarchive.org"),
+    ("/dandiset", "https://gui.dandiarchive.org/#/dandiset"),
     (
-        f"{REDIRECTOR}/dandiset/000003",
+        "/dandiset/000003",
         "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
     #(
-    #    f"{REDIRECTOR}/dandiset/000003/???",
+    #    "/dandiset/000003/???",
     #    "https://gui.dandiarchive.org/#/dandiset/???",
     #),
 ])
@@ -22,14 +21,13 @@ def test_redirect(req_url, resp_url):
     r.raise_for_status()
     assert r.url == resp_url
 
-
 @pytest.mark.parametrize('req_url,resp_url', [
     (
-        f"{REDIRECTOR}/dandiset/000003",
+        "/dandiset/000003",
         "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
     #(
-    #    f"{REDIRECTOR}/dandiset/000003/???",
+    #    "/dandiset/000003/???",
     #    "https://gui.dandiarchive.org/#/dandiset/???",
     #),
 ])
@@ -41,6 +39,6 @@ def test_redirect_head(req_url, resp_url):
     assert r.text == ''
 
 def test_redirect_nonexistent_dandiset():
-    _, r = app.test_client.get(f"{REDIRECTOR}/dandiset/{NEXIST_DANDI_ID}")
+    _, r = app.test_client.get(f"/dandiset/{NEXIST_DANDI_ID}")
     assert r.status_code == 404
     assert r.text == f"dandi:{NEXIST_DANDI_ID} not found."
