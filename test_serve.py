3c2d3a13459f9d6e33a9083450845149521ff9a9
John T. Wodder II <git@varonathe.org>
2020-08-07 12:55:20 -0400

    Add test of nonexistent Dandiset + version

--- test_serve.py
+++ test_serve.py
@@ -12,8 +12,8 @@ NEXIST_DANDI_ID = "999999"
         "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
     (
-    "/dandiset/000003/0.20200703.1040",
-    "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
+        "/dandiset/000003/0.20200703.1040",
+        "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
 ])
 def test_redirect(req_url, resp_url):
@@ -27,8 +27,8 @@ def test_redirect(req_url, resp_url):
         "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
     (
-    "/dandiset/000003/0.20200703.1040",
-    "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
+        "/dandiset/000003/0.20200703.1040",
+        "https://gui.dandiarchive.org/#/dandiset/5e6eb2b776569eb93f451f8d",
     ),
 ])
 def test_redirect_head(req_url, resp_url):
@@ -42,3 +42,8 @@ def test_redirect_nonexistent_dandiset():
     _, r = app.test_client.get(f"/dandiset/{NEXIST_DANDI_ID}")
     assert r.status_code == 404
     assert r.text == f"dandi:{NEXIST_DANDI_ID} not found."
+
+def test_redirect_nonexistent_dandiset_version():
+    _, r = app.test_client.get(f"/dandiset/{NEXIST_DANDI_ID}/0.20200703.1040")
+    assert r.status_code == 404
+    assert r.text == f"dandi:{NEXIST_DANDI_ID}/0.20200703.1040 not found."
