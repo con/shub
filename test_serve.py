ed65e07a1facc1cf9cf92534b0d7dca8f3d0184c
Yaroslav Halchenko <debian@onerussian.com>
2020-08-18 12:06:58 -0400

    Adjust the test for hosted minimal version

--- test_serve.py
+++ test_serve.py
@@ -64,7 +64,7 @@ def test_server_info():
     r.raise_for_status()
     assert r.json == {
         "version": "1.0.0",
-        "cli-minimal-version": "0.5.0",
+        "cli-minimal-version": "0.6.0",
         "cli-bad-versions": [],
         "services": {
             "girder": {"url": "https://girder.dandiarchive.org"},
