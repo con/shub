766ded181ba9309d00c06bd52c1c4b250be810c5
Yaroslav Halchenko <debian@onerussian.com>
2021-04-19 14:34:17 -0400

    adjusted verification script - some images are under sha256 checksum directory

--- _tools_/verify_md5.sh
+++ _tools_/verify_md5.sh
@@ -3,13 +3,30 @@
 set -eu
 
 for d in */*/*/*; do
-    img=$(/bin/ls $d/*.si[mf]*)
+    img=$(/bin/ls $d/*.si[mf]* 2>/dev/null)
     if [ -z "$img" ]; then
-        echo "absent image under $d"
-        exit 1
+        echo "absent sif or simg image under $d - skipping"
+        continue
     fi
     imgname=$(basename $img)
-    commit=$(dirname $img|xargs basename)
-    repo=$(dirname $img|xargs dirname | xargs basename)
-    org=$(dirname $img|xargs dirname | xargs dirname | xargs basename)
+    checksum=$(dirname $img|xargs basename)
+    #repo=$(dirname $img|xargs dirname | xargs basename)
+    #org=$(dirname $img|xargs dirname | xargs dirname | xargs basename)
+    if [ "${#checksum}" -ge 48 ]; then
+        # sha256 - needs to be computed
+        sha256=$(sha256sum $img|awk '{print $1;}')
+        if [ "$sha256" != "$checksum" ]; then
+            echo "ERROR: $img $checksum. Computed sha256=$sha256";
+            exit 1
+        else
+            echo "$img sha256 $checksum ok"
+        fi
+    else
+        # md5
+        if readlink -f "$img" | grep -e "--$checksum" -q ; then
+            echo "$img md5 ok"
+        else
+            echo "ERROR: $img $checksum "; readlink -f $img; exit 1;
+        fi
+    fi
 done
