ffa4c8be96d3cfc2084bb2b3131613bd71e93279
Yaroslav Halchenko <debian@onerussian.com>
2021-04-19 12:22:53 -0400

    Complete script to inject legacy (and verify md5 while at it)
    So we do not need to do all the cleanups after

--- _tools_/inject_legacy.sh
+++ _tools_/inject_legacy.sh
@@ -3,13 +3,28 @@
 set -eu
 
 for d in singularityhub-legacy/github.com/*/*/*; do
+    echo "I: $d"
     img=$(/bin/ls $d/*.img.gz)
     if [ -z "$img" ]; then
         echo "absent image under $d"
         exit 1
     fi
-    imgname=$(basename $img)
+    imgfile=$(basename $img)
+    imgname=${imgfile//.img.gz}  # md5sum of gunzip'ed content
     commit=$(dirname $img|xargs basename)
     repo=$(dirname $img|xargs dirname | xargs basename)
     org=$(dirname $img|xargs dirname | xargs dirname | xargs basename)
+    commitdir=$org/$repo/$commit
+    imgdir=$org/$repo/$commit/$imgname
+    if [ -e $imgdir ]; then
+        echo "$imgdir exists"
+        exit 1
+    fi
+    emd5=$(md5sum <(zcat $img ) | awk '{print $1;}')
+    if [ "$emd5" != "$imgname" ]; then
+      echo "$emd5 != $imgname"
+      exit 1
+    fi
+    mkdir -p $commitdir
+    mv $(dirname $img) $imgdir
 done
