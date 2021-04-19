baa8dfffdded70b6ecd2c6d4542d9910da70eac3
Yaroslav Halchenko <debian@onerussian.com>
2021-04-19 12:07:01 -0400

    cleanup: remove builds without images, and empty subdirs

--- _tools_/cleanup.sh
+++ _tools_/cleanup.sh
@@ -8,4 +8,12 @@ for d in */*/*/*; do
       ls -la $d/
       git rm -fr $d
     fi
+    img=$(/bin/ls $d/*.si[mf]* 2>/dev/null || echo '')
+    if [ -z "$img" ]; then
+        echo "Removing $d: absent image file"
+        ls -lL $d/
+        git rm -fr $d
+    fi
 done
+# remove empty dirs git does not care about
+rmdir */*/*/* */*/* */* 2>/dev/null || :
