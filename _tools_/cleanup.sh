5f69662487e822353b887793fb62c1cf814768db
Yaroslav Halchenko <debian@onerussian.com>
2021-04-20 19:57:52 -0400

    do allow for git rm to fail

--- _tools_/cleanup.sh
+++ _tools_/cleanup.sh
@@ -7,14 +7,14 @@ for d in */*/*/*; do
     if ! /bin/ls $d/ | grep -q . ; then
       echo "Removing $d since no files of interest. Has only:"
       ls -laL $d/
-      git rm -fr $d
+      git rm -fr $d || :
       continue
     fi
     img=$(/bin/ls $d/*.si[mf]* 2>/dev/null || /bin/ls $d/*.img.gz 2>/dev/null || echo '')
     if [ -z "$img" ]; then
         echo "Removing $d: absent image file"
         ls -laL $d/
-        git rm -fr $d
+        git rm -fr $d || echo "must have had no files"
     fi
 done
 # remove empty dirs git does not care about
